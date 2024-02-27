d520d936e86a15043f395f24908d140d2320ba02
Valentin_Silivankou <litin20011995@gmail.com>
2024-02-27 11:53:15 +0300

    first commit

---
+++ src/js/__tests__/getLevel.test.js
@@ -0,0 +1,29 @@
+// import {
+//   expect, jest, test, beforeEach,
+// } from '@jest/globals';
+import getLevel from '../getLevel';
+import fetchData from '../http';
+
+jest.mock('../http');
+
+beforeEach(() => {
+  jest.resetAllMocks();
+});
+
+test('correct-url-test', () => {
+  fetchData.mockReturnValueOnce('{}');
+  getLevel(500);
+  expect(fetchData).toHaveBeenCalledWith('https://server/user/500');
+});
+
+test('get-ok-test', () => {
+  fetchData.mockReturnValueOnce({ status: 'ok', level: '100' });
+  const result = getLevel(1);
+  expect(result).toBe('Ваш текущий уровень: 100');
+});
+
+test('false-status-test', () => {
+  fetchData.mockReturnValueOnce({ status: 'false' });
+  const result = getLevel(1);
+  expect(result).toBe('Информация об уровне временно недоступна');
+});
---
+++ src/js/http.js
@@ -0,0 +1,5 @@
+// Демо-реализация функции fetchData (модуль http):
+// eslint-disable-next-line no-unused-vars
+export default function fetchData(url) {
+  throw new Error('Mock this!');
+}
